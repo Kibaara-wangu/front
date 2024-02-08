3a31c659224a4b133841e293c0039464169c5595
kibaara-wangu <kibaarawanguggmail.com>
2024-02-08 07:27:23 +0300

    added students records

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name: String = "estherwanjiru"
+    println(name)
+    var age: Byte = 22
+    println(age)
+    var number: String = "0723567876"
+    println(number)
+    var weight: Byte = 60
+    println(weight)
+    var citizenship: Boolean = true
+    println(true)
+
+}
